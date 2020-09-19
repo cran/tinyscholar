afc58b67ab85cb23f94b43043bf66475e5f9f5a7
Shixiang Wang <w_shixiang@163.com>
2020-09-19 14:10:03 +0000

    version 0.1.1

--- inst/doc/tinyscholar.R
+++ inst/doc/tinyscholar.R
@@ -7,8 +7,17 @@ knitr::opts_chunk$set(
 ## -----------------------------------------------------------------------------
 library(tinyscholar)
 
-## -----------------------------------------------------------------------------
-scholar_search("Shixiang Wang")
+## ---- eval=FALSE--------------------------------------------------------------
+#  scholar_search("Shixiang Wang")
+#  #> Searching author Shixiang Wang
+#  #> Using API server: https://api.scaleserp.com
+#  #> Using Shixiang's personal API key, only 125 free searches per month for all packages users
+#  #> Search times used: 38
+#  #> Search times left: 87
+#  #>             id
+#  #> 1 FvNp0NkAAAAJ
+#  #>                                                                                               desc
+#  #> 1 Wang, Shixiang (王诗翔)ShanghaiTech. UniversityVerified email at shanghaitech.edu.cnCited by 127
 
 ## -----------------------------------------------------------------------------
 profile <- tinyscholar("FvNp0NkAAAAJ")
